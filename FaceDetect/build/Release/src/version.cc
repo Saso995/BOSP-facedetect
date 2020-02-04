393343f1576dd4a624d3f45c875c4801f15dda91
Salvatore Bova <salvatore_bova@hotmail.it>
2020-02-04 23:30:53 +0100

    final version

---
+++ FaceDetect/build/Release/src/version.cc
@@ -0,0 +1,19 @@
+/**
+ *       @file  version.cc
+ *      @brief  The FaceDetect BarbequeRTRM application
+ *
+ * Description: to be done...
+ *
+ *     @author  Name Surname (nickname), your@email.com
+ *
+ *     Company  Politecnico di Milano
+ *   Copyright  Copyright (c) 20XX, Name Surname
+ *
+ * This source code is released for free distribution under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ * =====================================================================================
+ */
+
+#define GIT_SHA1 "HEAD-HASH-NOTFOUND"
+const char *g_git_version = GIT_SHA1;
+
